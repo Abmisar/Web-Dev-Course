9c33b28ea72ec238402ee5f9de23a9b729329359
Ab <abdullahwalid18@gmail.com>
2025-07-30 22:12:24 +0300

    Final Updates

--- Backend/Section 26/index.js
+++ Backend/Section 26/index.js
@@ -3,25 +3,31 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 app.get("/", (req,res) => {
     res.render("index.ejs");
 });
 
-app.post("/reviews", (req,res) => {
-    res.render("reviews.ejs");
-})
+app.get("/favorite", (req,res) => {
+    res.render("favorite.ejs");
+});
 
-app.post("/500days", (req,res) =>{
-    res.render("500days.ejs")
-})
+app.get("/500days", (req,res) =>{
+    res.render("500days.ejs");
+});
 
-app.post("/aftersun", (req,res) =>{
-    res.render("aftersun.ejs")
-})
+app.get("/aftersun", (req,res) =>{
+    res.render("aftersun.ejs");
+});
 
-app.use(express.static("public"));
+app.get("/four", (req,res) =>{
+    res.render("four.ejs");
+});
 
+app.get("/about", (req,res) =>{
+    res.render("about.ejs");
+});
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}.`);
