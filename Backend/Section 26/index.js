3b64d860d1266b904c5831c8606c8a9a31b4be4e
Ab <abdullahwalid18@gmail.com>
2025-07-30 17:27:21 +0300

    Add remaining images and updated views

--- Backend/Section 26/index.js
+++ Backend/Section 26/index.js
@@ -12,7 +12,16 @@ app.post("/reviews", (req,res) => {
     res.render("reviews.ejs");
 })
 
+app.post("/500days", (req,res) =>{
+    res.render("500days.ejs")
+})
+
+app.post("/aftersun", (req,res) =>{
+    res.render("aftersun.ejs")
+})
+
 app.use(express.static("public"));
+
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}.`);
